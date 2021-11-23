5d1d1d5410cc40abf1f84d7b09f2b451154cf950
Hana Mengistu <hmengistu3@student.cccs.edu>
2021-11-23 01:32:12 -0700

    Merge branch 'master' of https://github.com/Miku2017/Demo_DatabaseProject

---
+++ Demo_DatabaseProject/MergeStatement.sql
@@ -0,0 +1,29 @@
+﻿-- Create Source table
+Create Table dbo.Employee_S
+(
+ Empcode  int identity (1001,1) ,
+ Empname  varchar(25)
+)
+
+--Insert into Source table 
+
+	Insert into dbo.Employee_S (Empname) 
+	Values ('Lulit'),('Belay'),('William')
+
+-- Create Target table
+Create Table dbo.Employee_T
+(
+ EmpId       int Identity(1,1) Not Null ,
+ Empcode     int Not Null ,
+ Empname     varchar(25) Not Null ,
+ StartDate   datetime2 Null,
+ EndtDate    datetime2  Null,
+ createdby   varchar(50) Default suser_sname(),
+ Createdon   varchar(50) Default SYSDATETIME(),
+ Modifyby    varchar(50) Null,
+ Modifiedon  datetime2 Null,
+ Constraint Pk_Employee_T_EmpId Primary Key (EmpId)
+);
+
+Select * from Employee_S
+Select * from Employee_T
